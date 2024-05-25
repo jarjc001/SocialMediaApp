098829d2fab32fdd3b26052b8269e996f5cde2ab
Christian Jarjat <jarjatc@gmail.com>
2024-05-25 23:12:49 +0100

    starting microservice part 2

--- src/main/java/com/SocialMediaAppUser/SocialMediaUserApplication.java
+++ src/main/java/com/SocialMediaAppUser/SocialMediaUserApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class App {
+public class SocialMediaUserApplication {
     public static void main(String[] args) {
 
-        SpringApplication.run(App.class, args);
+        SpringApplication.run(SocialMediaUserApplication.class, args);
     }
 }
