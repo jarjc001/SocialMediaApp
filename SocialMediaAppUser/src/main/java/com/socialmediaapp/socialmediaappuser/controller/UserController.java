9613388f2fbbb247ea44347abfabcb7222c0c701
Christian Jarjat <jarjatc@gmail.com>
2024-05-27 17:08:25 +0100

    completed tests for get user, need to finish tests for delete user and update

--- SocialMediaAppUser/src/main/java/com/socialmediaapp/socialmediaappuser/controller/UserController.java
+++ SocialMediaAppUser/src/main/java/com/socialmediaapp/socialmediaappuser/controller/UserController.java
@@ -1,13 +1,26 @@
 package com.socialmediaapp.socialmediaappuser.controller;
 
 import com.socialmediaapp.socialmediaappuser.dao.UserDao;
+import com.socialmediaapp.socialmediaappuser.dto.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/user")
 public class UserController {
     @Autowired
     UserDao users;
+
+    @GetMapping("/{username}")
+    public User getUser(@PathVariable("username") String username){
+        return new User(username,"HashPassword".toCharArray(),"testEmail","testfullname" );
+
+    }
+
 //
 //
 //
